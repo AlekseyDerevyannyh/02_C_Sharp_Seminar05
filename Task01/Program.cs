e921a2f87acf8adf67e5fc5875547339a1eb3151
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-20 23:03:50 +0300

    add 2 Task array from string

---
+++ Task01/Program.cs
@@ -0,0 +1,33 @@
+﻿// **Задача 35:** Задайте одномерный массив из 123 случайных чисел. Найдите количество
+// элементов массива, значения которых лежат в отрезке [10,99].
+// *Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123*
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+using System;
+using static System.Console;
+Clear();
+
+int[] Array = GetRandomArray(123, 0, 200);
+WriteLine($"[{String.Join(", ", Array)}]");
+int count = Count(Array, 10, 99);
+Write(count);
+
+
+
+int [] GetRandomArray (int size, int minValue, int maxValue) {
+	int[] result = new int[size];
+	for (int i = 0; i < size; i ++) {
+		result[i] = new Random().Next(minValue, maxValue + 1);
+	}
+	return result;
+}
+
+int Count (int[] array, int min, int max) {
+	int result = 0;
+	for (int i = 0; i < array.Length; i ++) {
+		if (array[i] >= min && array[i] <= max) 	result ++;
+	}
+	return result;
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,29 @@
+﻿// **Задача 37:** Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите элементы массива через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(", ", array)}]");
+WriteLine($"[{String.Join(", ", (MultiPair(array)))}]");
+
+int[] GetArrayFromString (string arrayStr) {
+	string[] arS = arrayStr.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+	int[] result = new int[arS.Length];
+	for(int i = 0; i < arS.Length; i ++) {
+		result[i]=int.Parse(arS[i]);
+	}
+	return result;
+}
+
+int[] MultiPair (int[] _array) {
+	int[] result = new int[_array.Length % 2 == 0 ? _array.Length / 2 : _array.Length / 2 + 1];
+	for (int i = 0; i < (_array.Length % 2 == 0 ? result.Length: result.Length - 1); i ++) {
+		result[i] = _array[i] * _array[_array.Length - i - 1];
+	}
+	if ((_array.Length % 2) != 0)	result[result.Length - 1] = _array[_array.Length / 2];
+	return result;
+}
